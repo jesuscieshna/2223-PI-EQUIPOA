6d75060ec3ad2a4a0271d662ec4b3c3571c00999
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2023-04-29 12:42:04 +0200

    loader

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,3 +1,16 @@
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loader").classList.toggle("loader2")
+        document.getElementById("html").classList.remove("block-sroll");
+        document.getElementById("html").classList.add("block-sroll2");
+   
+    },2000);
+ 
+   
+
+})
+
 const $openClose = document.getElementById('open-close'), $aside = document.getElementById("aside");
  
 
@@ -5,4 +18,48 @@ $openClose.addEventListener("click",()=> {
     $aside.classList.toggle("desplegar")
     
 })
+
+
+const datos= {
+   juan:{
+    nombre:"Juan",
+    contraseña:"12345"
+
+   }
+}
+
+
+const $input_usuario= document.getElementById("input_usuario");
+const $input_contraseña = document.getElementById("input_contraseña");
+
+
+function verify_usuario() {
+        
+       Object.values(datos).forEach(usuario => {
+        if(($input_usuario.value==usuario["nombre"]) && ($input_contraseña.value==usuario["contraseña"])){
+            $input_usuario.style.backgroundColor="#7CFC00";
+            $input_contraseña.style.backgroundColor="#7CFC00";
+            setTimeout(() => {
+                window.location.href="./index.html"
+            }, 1000);
+            
+        }else{
+            $input_usuario.style.backgroundColor="#FF0000";
+            $input_contraseña.style.backgroundColor="#FF0000";
+        }
+        
+        
+        })
+       
+        
+
+       }
+
+       
+
+
+
+
+    
+
 
