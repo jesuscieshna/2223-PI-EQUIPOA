922dd3b55b88afbcd12c50c52a6843bca741197a
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2023-05-05 20:28:16 +0200

    .

--- Frontend/FilmRadar/src/App.jsx
+++ Frontend/FilmRadar/src/App.jsx
@@ -10,23 +10,23 @@ function App() {
   return (
     <>
       <header>
-      <Menu></Menu>
+        <Menu></Menu>
       </header>
-     
 
-        
-        <div className='cuerpo-principal'>
+
+
+      <div className='cuerpo-principal'>
         <video width={"100px"} src={Video}></video>
         <h1>asdasd</h1>
         <h2>asdasdasdqasdasd</h2>
         <div width={"20%"} ></div>
-        <MenuBot></MenuBot>
-        </div>
-       
-     
-        
-        
-    </>
+        <MenuBot></MenuBot>                                                       
+      </div>
+
+
+
+
+    </>                                         
   )
 }
 
