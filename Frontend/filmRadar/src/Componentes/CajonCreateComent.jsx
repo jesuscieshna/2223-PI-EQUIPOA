946001f4f54b0aa21ca63ec8ee63b018539a52a1
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2023-05-21 13:25:11 +0200

    .

--- Frontend/filmRadar/src/Componentes/CajonCreateComent.jsx
+++ Frontend/filmRadar/src/Componentes/CajonCreateComent.jsx
@@ -1,24 +1,37 @@
+import { useNavigate } from "react-router-dom"
 import svck from "../services/CookiesServices"
 import "./style/cajonCreateComent.css"
+
 export default function CajonCreateComent(props){
     console.log(props)
     
-    const user=svck.getUserNameForCookies
+    const user=svck.getUserNameForCookies;
+    
 
+    const cancelComent = function(){
+       
+        document.getElementById("cajon-createcomenet").style.display="none"
+    }
 
 
     return(
 
         <div id="cajon-createcomenet" className="cajon-createcomenet">
-            <form  className="form-createComent"> 
+            <div  className="form-createComent"> 
             <div>
                  <h1>Tu Comentario</h1>
                 <textarea type="text" className="textBox"     minLength={10} placeholder="Escribe tu comentario aqui" maxLength={255}></textarea>
+            
+             
+            </div>
+            <div className="botonesCometn">
+                <button className="cancelComent" onClick={cancelComent}>Cancelar</button> 
+                <button className="btnEnviar" onClick={null}>Enviar</button>
+            </div>
+        
+           
                
             </div>
-            <input className="btnEnviar" type="submit" value={"Enviar"}></input>
-               
-            </form>
         </div>
 
     )
--- Frontend/filmRadar/src/Componentes/comentario.jsx
+++ Frontend/filmRadar/src/Componentes/comentario.jsx
@@ -23,9 +23,6 @@ export default function Comentario() {
                  </p>
                  </div>
             </div>
-
-
-
         </Suspense>
 
 
